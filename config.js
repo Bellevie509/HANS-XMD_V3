50b003f7a19130b4a3c26902064008e06a182659
Bellevie509 <wadsonbot@gmail.com>
2025-04-24 07:30:00 -0400

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "https://whatsapp.com/channel/0029Vb2ebDF4dTnKini7ke0Q/260",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
